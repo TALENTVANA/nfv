1d86942d632ea187d9b30b355492ae9ee8c36886
Raghu Katti <rakatti@gmail.com>
2017-12-10 19:07:02 +0530

    removed push from script

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@ mvn package docker:build
 version=`cat VERSION`
 git add VERSION 
 git commit -m "version $version"
-git tag -a "$version" -m "version $version"
-git push origin master
-git push --tags
 sudo docker tag demovana/nfv:latest demovana/nfv:$version
 sudo docker push demovana/nfv:latest
 sudo docker push demovana/nfv:$version
