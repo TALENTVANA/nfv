d6c9cff36410a8d439af524b841eaf83429a5933
Raghu Katti <rakatti@gmail.com>
2017-12-10 19:58:17 +0530

    Renamed to nfv-demo

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mvn package docker:build
 version=`cat VERSION`
 git add VERSION 
 git commit -m "version $version"
-sudo docker tag demovana/nfv:latest demovana/nfv:$version
-sudo docker push demovana/nfv:latest
-sudo docker push demovana/nfv:$version
+sudo docker tag demovana/nfv-demo:latest demovana/nfv:$version
+sudo docker push demovana/nfv-demo:latest
+sudo docker push demovana/nfv-demo:$version
