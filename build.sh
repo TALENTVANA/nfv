2b2bdebd3e497da47ddd872bec6860bf63f0a009
Raghu Katti <rakatti@gmail.com>
2017-12-10 17:43:54 +0530

    Version Management

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 mvn clean install
 mvn package docker:build
-docker run --rm -v "$PWD":/app treeder/bump patch
+sudo docker run --rm -v "$PWD":/app treeder/bump patch
 version=`cat VERSION`
 git add VERSION 
 git commit -m "version $version"
 git tag -a "$version" -m "version $version"
 git push
 git push --tags
-docker tag demovana/nfv:latest demovana/nfv:$version
-docker push demovana/nfv:latest
-docker push demovana/nfv:$version
+sudo docker tag demovana/nfv:latest demovana/nfv:$version
+sudo docker push demovana/nfv:latest
+sudo docker push demovana/nfv:$version
